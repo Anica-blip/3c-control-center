92b1b318e136971689ff8518c50f4825b1fbc746
Anica <3c.innertherapy@gmail.com>
2025-09-14 19:41:24 +0100

    Create ContentCreationForm.tsx

---
+++ src/contentcomponent/ContentCreationForm.tsx
@@ -0,0 +1,5 @@
+import { EnhancedContentCreationForm } from './EnhancedContentCreationForm';
+
+// Re-export with correct name
+export { EnhancedContentCreationForm as ContentCreationForm };
+export default EnhancedContentCreationForm;
