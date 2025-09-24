ab121269b8e0677e7dcd229cb9a99877bc16f449
Anica <3c.innertherapy@gmail.com>
2025-09-24 20:15:33 +0100

    Create index.tsx

---
+++ src/schedulecomponent/index.tsx
@@ -0,0 +1,5 @@
+// /src/schedulecomponent/index.tsx - Export the working Schedule Component
+import ScheduleComponent from './ScheduleComponent';
+
+// Export as ScheduleComponentContent to match App.tsx import
+export default ScheduleComponent;
